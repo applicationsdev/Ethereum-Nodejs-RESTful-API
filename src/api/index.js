fd73b2ab6e26d5a08aa996650d9a91a296130f70
Applications Developer <25175797+applicationsdev@users.noreply.github.com>
2018-04-16 18:00:37 +0000

    Ether.js imported

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,12 @@
 import { Router } from 'express';
 
+// Import ethers.js library
+let ethers = require('ethers');
+
+// Connect to Eth network
+let providers = ethers.providers;
+let provider = providers.getDefaultProvider('ropsten');
+
 export default ({ config }) => {
 	let api = Router();
 
