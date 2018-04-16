932adea169f331ba91a09eb79404b549e252e455
Applications Developer <25175797+applicationsdev@users.noreply.github.com>
2018-04-16 17:56:26 +0000

    API routes customization - file cleared

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,7 @@
-import { version } from '../../package.json';
 import { Router } from 'express';
-import facets from './facets';
 
-export default ({ config, db }) => {
+export default ({ config }) => {
 	let api = Router();
 
-	// mount the facets resource
-	api.use('/facets', facets({ config, db }));
-
-	// perhaps expose some API metadata at the root
-	api.get('/', (req, res) => {
-		res.json({ version });
-	});
-
 	return api;
-}
+};
