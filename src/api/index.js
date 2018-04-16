0cb9720370b8f1e32ed4ef52e5fe11f94bfd28cb
Applications Developer <25175797+applicationsdev@users.noreply.github.com>
2018-04-16 19:23:05 +0000

    Ethereum networks master config feature created

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,29 @@
+/*
+   Ethereum ops via RESTful API with Node.js
+   
+   @applicationsdev
+   
+   GPLv3
+*/
+
+/* Imports */
+
 import { Router } from 'express';
 
 // Import Ethers.js library
 let ethers = require('ethers');
 
-// Connect to Eth network
+// Ethers.js module to connect to Eth networks
 let providers = ethers.providers;
-let provider = providers.getDefaultProvider('ropsten');
 
 // Ethers.js wallet module
 let Wallet = ethers.Wallet;
+
+// Ethereum master configuration feature
+import ethconfig from '../ethconfig.json';
+
+
+/* API Routes */
 
 export default ({ config }) => {
 	
@@ -36,7 +51,10 @@ export default ({ config }) => {
 
 	api.get('/getBalance/:address', (req, res) => {
 		
-		provider.getBalance(req.params.address).then(
+		// Choose network
+		let getBalanceProvider = providers.getDefaultProvider(ethconfig.network.API.routeGroup.TXN);
+		
+		getBalanceProvider.getBalance(req.params.address).then(
 			
 			// Promise has succeeded to resolve the request
 			(balance) => {
@@ -55,7 +73,7 @@ export default ({ config }) => {
 		let walletOfSender = new Wallet(req.params.privateKey);
 		
 		// Choose network
-		walletOfSender.provider = providers.getDefaultProvider('ropsten');
+		walletOfSender.provider = providers.getDefaultProvider(ethconfig.network.API.routeGroup.TXN);
 		
 		// TXN amount
 		let amount = ethers.utils.parseEther(req.params.amount);
